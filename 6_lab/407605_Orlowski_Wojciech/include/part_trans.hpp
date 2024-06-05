e4fe5733a62f53db06cd3479f3ba8d671a7d70e5
OrlowskiWojtek <owoj@vp.pl>
2024-06-05 21:11:03 +0200

    a co to moze byc - gitignore

--- 6_lab/407605_Orlowski_Wojciech/include/part_trans.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef PART_TRANS_HPP
-#define PART_TRANS_HPP
-
-void vector_rotation(double tx, double ty, double tz, double & x, double & y, double & z, double teta);
-void particle_translation(double & x1,double & y1, double & x2,double & y2, 
-				  double xr,double yr, double Rr, double xa,double ya, double Ra, 
-				  int & exist ,double & length);
-
-#endif
